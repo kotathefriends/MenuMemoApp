ffdf2a4d19ae23f87ece84eced359f2923a1245c
kotathefriends <5070kouta@gmail.com>
2023-10-17 16:45:05 +0900

    Initial Commit

---
+++ MenuMemoApp/MenuMemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuMemoAppApp.swift
+//  MenuMemoApp
+//
+//  Created by KOTA TAKAHASHI on 2023/10/17.
+//
+
+import SwiftUI
+
+@main
+struct MenuMemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
